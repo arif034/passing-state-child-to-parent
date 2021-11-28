51707b7aeffbead9f170fc464156f30143e92ca0
arif034 <45455917+arif034@users.noreply.github.com>
2021-11-28 17:48:35 +0530

    commit-1 commit-1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import "./styles.css";
 import Child from "./Child";
 import { Component } from "react";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "Parent Message"
+    };
+  }
   render() {
     return (
       <div className="App">
-        <Child />
+        <Child message={this.state.message} />
       </div>
     );
   }
--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,37 @@
 import { Component } from "react";
 
 class Child extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      childMessage: this.props.message
+    };
+  }
+  onClickOfButton(props) {
+    if (this.state.childMessage === this.props.message) {
+      this.setState({
+        childMessage: "Child Message"
+      });
+      console.log(this.state.childMessage);
+    } else {
+      this.setState({
+        childMessage: "Parent Message"
+      });
+      console.log(this.state.childMessage);
+    }
+  }
+
   render() {
-    return <div>This is child component</div>;
+    return (
+      <div>
+        <br />
+        <br />
+        <button onClick={() => this.onClickOfButton()}>Click Me</button>
+        <br />
+        <br />
+        {this.state.childMessage}
+      </div>
+    );
   }
 }
 export default Child;
