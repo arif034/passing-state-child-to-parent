ba718156f7c22ac69f3c6599f1cf7a9289dfd299
arif034 <45455917+arif034@users.noreply.github.com>
2021-11-28 16:43:33 +0530

    passing state from child to parent passing state from child to parent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./styles.css";
-
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+import Child from "./Child";
+import { Component } from "react";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Child />
+      </div>
+    );
+  }
 }
+export default App;
---
+++ src/Child.js
@@ -0,0 +1,8 @@
+import { Component } from "react";
+
+class Child extends Component {
+  render() {
+    return <div>This is child component</div>;
+  }
+}
+export default Child;
